71376b7142e4ebf8a73830eab10f80b6918523e0
vinyborges <viny.bdj@gmail.com>
2023-02-08 23:55:06 -0800

    commit tree 84df98b36bbb00ff4c98b154f07739a7758404ec

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+
+function saturdayFun(fill='roller-skate'){
+let msgStirng;
+msgStirng = `This Saturday, I want to ${fill}!`;
+
+
+return msgStirng;
+}
+saturdayFun('bathe my dog')
+
+function mondayWork(fill='go to the office'){
+   let msgStirng;
+   msgStirng = `This Monday, I will ${fill}.`;
+   return msgStirng;
+}
+mondayWork();
+
+function wrapAdjective(){
+
+}
+
